886c34ed6109825b3cb8dad9bcbba74a89a3e240
GeorgeZhang744 <georgezhang744@gmail.com>
2024-09-20 13:04:00 -0400

    complete project 1

---
+++ project1_listicle_part1/server/public/scripts/boss.js
@@ -0,0 +1,24 @@
+const renderBoss = async () => {
+  const requestedID = parseInt(window.location.href.split("/").pop());
+
+  const response = await fetch("/bosses");
+  const data = await response.json();
+
+  const bossContent = document.getElementById("boss-content");
+  let boss = data.find((boss) => boss.id === requestedID);
+
+  if (boss) {
+    document.getElementById("image").src = `/bosses/assets/${boss.image}`;
+    document.getElementById("name").textContent = boss.name;
+    document.getElementById("health").textContent = "Health: " + boss.health;
+    document.getElementById("location").textContent = "Location: " + boss.location;
+    document.getElementById("description").textContent = boss.description;
+    document.title = `Hollow Knight Boss - ${boss.name}`;
+  } else {
+    const message = document.createElement("h2");
+    message.textContent = "No Bosses Found 😞";
+    bossContent.appendChild(message);
+  }
+};
+
+renderBoss();
