4e4069412410387f17933e88607ab6b6a369e500
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-04 14:29:52 -0400

    feat(client): complete EventsByLocationPage

--- project3_virtual_community_space/client/src/pages/EventsByLocationPage.jsx
+++ project3_virtual_community_space/client/src/pages/EventsByLocationPage.jsx
@@ -1,8 +1,47 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import EventCard from "../components/EventCard";
 
 export default function EventsByLocationPage() {
+  const params = useParams();
+  const { eventLocation } = params;
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch(`/events/location/${eventLocation}`);
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        setEvents(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchEvents();
+  }, [eventLocation]);
+
   return (
-    <div>EventsByLocationPage</div>
-  )
+    <div className="container mx-auto p-4">
+      <h1 className="text-5xl font-bold mb-6">Event List</h1>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {events.map((event) => (
+          <li key={event.id} className="bg-white shadow-md rounded-lg p-4">
+            <EventCard key={event.id} event={event} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
