81191dda6a80231411b4f3aa86d226e8fb1b5a36
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-04 14:57:29 -0400

    fix(client): fix inconsistent width issue in NavBar

--- project3_virtual_community_space/client/src/components/NavBar.jsx
+++ project3_virtual_community_space/client/src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
-      <div className="flex flex-wrap justify-between items-center mx-auto p-4">
+    <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900 w-full fixed top-0 left-0 z-10">
+      <div className="flex flex-wrap justify-between items-center max-w-screen-xl mx-auto p-4">
         <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white">Virtual Community Space</span>
         <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
