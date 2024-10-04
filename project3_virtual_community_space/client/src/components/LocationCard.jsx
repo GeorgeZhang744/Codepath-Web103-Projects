3b3d1eeccb87f98f38f7ed3f4c3ef30d2d46d469
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-04 14:28:51 -0400

    feat(client): add link to EventsByLocationPage to the button in location card

--- project3_virtual_community_space/client/src/components/LocationCard.jsx
+++ project3_virtual_community_space/client/src/components/LocationCard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function LocationCard(props) {
   const { locationName, imageURL } = props;
@@ -18,12 +19,14 @@ export default function LocationCard(props) {
         <h6 className="mb-2 text-slate-800 text-xl font-semibold">{locationName}</h6>
       </div>
       <div className="px-4 pb-4 pt-0 mt-2">
-        <button
-          className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          type="button"
-        >
-          View Events
-        </button>
+        <Link to={`/events/location/${locationName}`}>
+          <button
+            className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+          >
+            View Events
+          </button>
+        </Link>
       </div>
     </div>
   );
