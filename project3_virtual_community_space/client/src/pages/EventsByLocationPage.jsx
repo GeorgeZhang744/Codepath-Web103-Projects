aa783baa9b485b7e14eb4ee9b6f04344d5a34fcd
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-04 14:58:06 -0400

    style(client): slightly change in styling

--- project3_virtual_community_space/client/src/pages/EventsByLocationPage.jsx
+++ project3_virtual_community_space/client/src/pages/EventsByLocationPage.jsx
@@ -33,7 +33,7 @@ export default function EventsByLocationPage() {
   }, [eventLocation]);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="w-full mx-auto px-8 py-20">
       <h1 className="text-5xl font-bold mb-6">Event List</h1>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {events.map((event) => (
--- project3_virtual_community_space/client/src/pages/EventsPage.jsx
+++ project3_virtual_community_space/client/src/pages/EventsPage.jsx
@@ -28,12 +28,12 @@ export default function EventsPage() {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="w-full mx-auto px-8 py-20">
       <h1 className="text-5xl font-bold mb-6">Event List</h1>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {events.map((event) => (
           <li key={event.id} className="bg-white shadow-md rounded-lg p-4">
-            <EventCard key={event.id} event={event}/>
+            <EventCard key={event.id} event={event} />
           </li>
         ))}
       </ul>
