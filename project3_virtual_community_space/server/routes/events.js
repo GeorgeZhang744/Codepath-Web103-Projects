f8d29f9cbf763fd67eef50265a51ee2d428e4a66
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-03 22:10:26 -0400

    feat(router): add routes to the server
    routes added:
    - GET "/events": get all events
    - GET "/events/locations": get all distinct locations available for events
    - GET "/events/:eventID": get a single event with eventID
    - GET "/events/location/:eventLocation": get all events locating at eventLocation

---
+++ project3_virtual_community_space/server/routes/events.js
@@ -0,0 +1,66 @@
+import express from "express";
+import { pool } from "../config/database.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const getAllEventsQuery = `
+    SELECT * FROM events ORDER BY id ASC
+  `;
+
+  try {
+    const result = await pool.query(getAllEventsQuery);
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+});
+
+router.get("/locations", async (req, res) => {
+  const getAllLocationsQuery = `
+    SELECT DISTINCT location FROM events
+  `;
+
+  try {
+    const result = await pool.query(getAllLocationsQuery);
+    res.status(200).json(result.rows.map(data => data.location));
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+});
+
+router.get("/:eventID", async (req, res) => {
+  const eventID = req.params.eventID;
+
+  const getEventQuery = `
+    SELECT * FROM events WHERE id = $1
+  `;
+
+  const value = [eventID]
+
+  try {
+    const result = await pool.query(getEventQuery, value);
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+});
+
+router.get("/location/:eventLocation", async (req, res) => {
+  const eventLocation = req.params.eventLocation;
+
+  const getEventsByLocationQuery = `
+    SELECT * FROM events WHERE location = $1
+  `;
+
+  const value = [eventLocation];
+
+  try {
+    const result = await pool.query(getEventsByLocationQuery, value);
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+});
+
+export default router;
