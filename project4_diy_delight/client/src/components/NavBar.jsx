f46b24c1a83566edc389a19d0e46bec1341310ba
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-13 01:33:56 -0400

    style(navbar): slightly change styling

--- project4_diy_delight/client/src/components/NavBar.jsx
+++ project4_diy_delight/client/src/components/NavBar.jsx
@@ -8,19 +8,20 @@ const Navbar = () => {
     <nav className="navbar bg-neutral text-white top-0 left-0 right-0 z-50 shadow-lg">
       <div className="container mx-auto flex justify-between items-center py-6">
         <div className="flex items-center">
-          <span className="text-3xl font-bold">Bolt Bucket</span>
+          <h1 className="flex text-4xl font-bold">
+            <div aria-label="t-shirt" className="mb-3">
+              👕
+            </div>
+            <div className="my-auto">DIY T-Shirts</div>
+          </h1>
         </div>
 
         <div className="flex items-center space-x-4">
           <Link to="/">
-            <button className="btn bg-primary text-white text-xl px-6 py-2 rounded-sm hover:bg-black border-none">
-              CUSTOMIZE
-            </button>
+            <button className="btn bg-primary text-white text-xl px-6 py-2 rounded-md hover:bg-black">CUSTOMIZE</button>
           </Link>
           <Link to={"/shirts"}>
-            <button className="btn bg-primary text-white text-xl px-6 py-2 rounded-sm hover:bg-black border-none">
-              VIEW SHIRTS
-            </button>
+            <button className="btn bg-primary text-white text-xl px-6 py-2 rounded-md hover:bg-black">VIEW SHIRTS</button>
           </Link>
         </div>
       </div>
