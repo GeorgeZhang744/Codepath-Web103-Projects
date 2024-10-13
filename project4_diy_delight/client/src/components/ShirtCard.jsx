d9d95c2c044797a82eeaecf04d61be3c01a4f1f1
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-13 16:38:01 -0400

    feat(client): add ShirtCard component for displaying a stored T-shirt, and complete AllShirts page that displays all stored T-shirts

--- project4_diy_delight/client/src/components/ShirtCard.jsx
+++ project4_diy_delight/client/src/components/ShirtCard.jsx
@@ -1,10 +1,72 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
-function ShirtCard() {
+import { Link } from "react-router-dom";
+
+function ShirtCard(props) {
+  const { shirt, price } = props;
+
   return (
-    <div>ShirtCard</div>
-  )
+    <div className="bg-gray-800 max-w-screen-2xl mx-auto h-60 text-white rounded-lg px-20 shadow-lg flex justify-between items-center cursor-pointer transform transition-transform duration-300 hover:scale-105">
+      <div className="flex flex-col justify-center space-y-4">
+        <h1 className="flex text-2xl font-bold">
+          <div aria-label="t-shirt" className="mb-2 mr-1">
+            👕
+          </div>
+          <div className="my-auto">{shirt.name}</div>
+        </h1>
+
+        <div className="grid grid-cols-2 gap-4 gap-x-20 text-lg text-center">
+          <p className="flex">
+            <span role="img" aria-label="size" className="mr-2">
+              📏
+            </span>
+            <p>
+              <strong>Size:</strong> {shirt.size}
+            </p>
+          </p>
+          <p className="flex">
+            <span role="img" aria-label="color" className="mr-2">
+              🎨
+            </span>
+            <p>
+              <strong>Color:</strong> {shirt.color}
+            </p>
+          </p>
+
+          <p className="flex">
+            <span role="img" aria-label="design" className="mr-2">
+              🖊️
+            </span>
+            <p>
+              <strong>Design:</strong> {shirt.design}
+            </p>
+          </p>
+          <p className="flex">
+            <span role="img" aria-label="material" className="mr-2">
+              🧵
+            </span>
+            <p>
+              <strong>Material:</strong> {shirt.material}
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-end space-y-4">
+        <div className="flex items-center space-x-2 text-3xl font-bold">
+          <span role="img" aria-label="money bag">
+            💰
+          </span>
+          <span>${price.toFixed(2)}</span>
+        </div>
+        <Link to={`/shirts/${shirt.id}`}>
+          <button className="btn bg-red-600 text-white text-lg font-bold px-4 py-2 rounded-md hover:bg-black">DETAILS</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default ShirtCard
+export default ShirtCard;
--- project4_diy_delight/client/src/pages/AllShirts.jsx
+++ project4_diy_delight/client/src/pages/AllShirts.jsx
@@ -1,10 +1,76 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+
+import ShirtCard from "../components/ShirtCard";
 
 function AllShirts() {
+  const basePrice = 15;
+  const [shirts, setShirts] = useState([]);
+  const [designOptions, setDesignOptions] = useState([]);
+  const [materialOptions, setMaterialOptions] = useState([]);
+
+  useEffect(() => {
+    const getShirts = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/shirts");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        setShirts(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const getPriceOptions = async () => {
+      try {
+        const response1 = await fetch("http://localhost:3001/api/options/designs");
+        const response2 = await fetch("http://localhost:3001/api/options/materials");
+
+        if (!response1.ok && !response2) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data1 = await response1.json();
+        const data2 = await response2.json();
+
+        setDesignOptions(data1);
+        setMaterialOptions(data2);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getShirts();
+    getPriceOptions();
+  }, []);
+
   return (
-    <div>AllShirts</div>
-  )
+    <div className="bg-gray-500 text-white min-h-screen p-8 space-y-8">
+      {shirts.map((shirt, idx) => {
+        let price = basePrice;
+
+        designOptions.forEach((option) => {
+          if (option.design === shirt.design) {
+            price += option.price;
+          }
+        });
+
+        materialOptions.forEach((option) => {
+          if (option.material === shirt.material) {
+            price += option.price;
+          }
+        });
+
+        return <ShirtCard key={idx} shirt={shirt} price={price} />;
+      })}
+    </div>
+  );
 }
 
-export default AllShirts
+export default AllShirts;
