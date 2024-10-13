f5d48f69f5d72ef567d531a8c3db652e0714682b
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-13 14:52:38 -0400

    feat(server): add an new route that retrieves all options at once

--- project4_diy_delight/server/routes/options.js
+++ project4_diy_delight/server/routes/options.js
@@ -3,6 +3,45 @@ import { pool } from "../config/database.js";
 
 const optionsRouter = express.Router();
 
+optionsRouter.get("/", async (req, res) => {
+  const getSizesQuery = `
+    SELECT size
+    FROM "size-options"
+  `;
+
+  const getColorsQuery = `
+    SELECT color
+    FROM "color-options"
+  `;
+
+  const getDesignsQuery = `
+    SELECT design, price
+    FROM "design-options"
+    ORDER BY price ASC
+  `;
+
+  const getMaterialsQuery = `
+    SELECT material, price
+    FROM "material-options"
+    ORDER BY price ASC
+  `;
+
+  try {
+    const sizeResult = await pool.query(getSizesQuery);
+    const colorResult = await pool.query(getColorsQuery);
+    const designResult = await pool.query(getDesignsQuery);
+    const materialResult = await pool.query(getMaterialsQuery);
+    res.status(200).json({
+      sizeOptions: sizeResult.rows,
+      colorOptions: colorResult.rows,
+      designOptions: designResult.rows,
+      materialOptions: materialResult.rows,
+    });
+  } catch (err) {
+    res.status(409).json({ error: err.message });
+  }
+});
+
 optionsRouter.get("/sizes", async (req, res) => {
   const getSizesQuery = `
     SELECT size
