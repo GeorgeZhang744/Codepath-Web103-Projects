fbd4ff07447530a6a4cdf9686bafc6d2d4b22149
GeorgeZhang744 <georgezhang744@gmail.com>
2024-10-13 14:52:05 -0400

    feat & style(client): complete AddShirt page
    feat:
    - allow user to select options for their T-shirt, and automatically calculate the price based on the selected options
    - add fetch to the server to stored the customized T-shirt
    - add warning message when user missed to select some of the options
    
    style:
    - update the styling of the optionCard such that the selected option has different color than the unselected ones

--- project4_diy_delight/client/src/components/OptionCard.jsx
+++ project4_diy_delight/client/src/components/OptionCard.jsx
@@ -3,23 +3,23 @@
 import React from "react";
 
 function OptionCard(props) {
-  const { options, optionName, isPriced } = props;
+  const { options, optionName, isPriced, selectedOptions, onSelect } = props;
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-6">
       {options.map((option, index) => {
-        const imagePath = `/src/assets/${option[optionName]}.png`;
-
+        // Use split and join trick to get rid of space in between words
+        const imagePath = `/src/assets/${option[optionName].split(" ").join("")}.png`;
         return (
           <div
             key={index}
-            className="card bg-primary text-white rounded-md p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105"
+            className={`card ${
+              selectedOptions[optionName] === option[optionName] ? "  bg-primary" : " bg-red-400"
+            }  text-white rounded-md p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105`}
+            onClick={() => onSelect(optionName, option[optionName])}
           >
             <div className="h-full bg-gray-300 mb-2">
-              <img
-                src={imagePath}
-                alt={`${option[optionName]} T-shirt`}
-                className="h-full w-full object-cover"
-              />
+              <img src={imagePath} alt={`${option[optionName]} T-shirt`} className="h-full w-full object-cover" />
             </div>
             <span className="text-xl text-center">{option[optionName]}</span>
             {isPriced && <p className="text-lg text-yellow-400 mt-2">+${option.price}</p>}
--- project4_diy_delight/client/src/pages/AddShirt.jsx
+++ project4_diy_delight/client/src/pages/AddShirt.jsx
@@ -1,43 +1,153 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 
 import OptionCard from "../components/OptionCard";
 
 const AddShirt = () => {
+  const basePrice = 15.0;
+  const baseSelectedOption = {
+    size: "",
+    color: "",
+    design: "",
+    material: "",
+  };
+
   const [activeTab, setActiveTab] = useState("Size");
   const [shirtName, setShirtName] = useState("");
-  const [price, setPrice] = useState(79.99); // Base price
+  const [options, setOptions] = useState({
+    sizeOptions: [],
+    colorOptions: [],
+    designOptions: [],
+    materialOptions: [],
+  });
+  const [price, setPrice] = useState(basePrice); // Base price
+  const [selectedOptions, setSelectedOptions] = useState(baseSelectedOption);
 
-  const sizeOptions = [{ size: "S" }, { size: "M" }, { size: "L" }, { size: "XL" }];
+  useEffect(() => {
+    const fetchOptions = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/options");
 
-  const colorOptions = [{ color: "Black" }, { color: "White" }, { color: "Blue" }, { color: "Red" }];
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
 
-  // Sample prices for design and material options
-  const designOptions = [
-    { design: "Logo", price: 10 },
-    { design: "Slogan", price: 5 },
-    { design: "CustomText", price: 15 },
-  ];
+        const data = await response.json();
 
-  const materialOptions = [
-    { material: "Cotton", price: 0 },
-    { material: "Polyester", price: 5 },
-    { material: "Blend", price: 10 },
-  ];
+        setOptions(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchOptions();
+  }, []);
+
+  const onSelectOption = (optionName, optionValue) => {
+    const newSelectedOptions = { ...selectedOptions };
+    newSelectedOptions[optionName] = optionValue;
+
+    let newPrice = basePrice;
+
+    options.designOptions.forEach((option) => {
+      if (option.design === newSelectedOptions.design) {
+        newPrice += option.price;
+      }
+    });
+
+    options.materialOptions.forEach((option) => {
+      if (option.material === newSelectedOptions.material) {
+        newPrice += option.price;
+      }
+    });
+
+    setSelectedOptions(newSelectedOptions);
+    setPrice(newPrice);
+  };
 
   // Function to render options based on the active tab
   const renderOptions = () => {
     switch (activeTab) {
       case "Size":
-        return <OptionCard options={sizeOptions} optionName={"size"} isPriced={false} />;
+        return (
+          <OptionCard
+            options={options.sizeOptions}
+            optionName={"size"}
+            isPriced={false}
+            selectedOptions={selectedOptions}
+            onSelect={onSelectOption}
+          />
+        );
       case "Color":
-        return <OptionCard options={colorOptions} optionName={"color"} isPriced={false} />;
+        return (
+          <OptionCard
+            options={options.colorOptions}
+            optionName={"color"}
+            isPriced={false}
+            selectedOptions={selectedOptions}
+            onSelect={onSelectOption}
+          />
+        );
       case "Design":
-        return <OptionCard options={designOptions} optionName={"design"} isPriced={true} />;
+        return (
+          <OptionCard
+            options={options.designOptions}
+            optionName={"design"}
+            isPriced={true}
+            selectedOptions={selectedOptions}
+            onSelect={onSelectOption}
+          />
+        );
       case "Material":
-        return <OptionCard options={materialOptions} optionName={"material"} isPriced={true} />;
+        return (
+          <OptionCard
+            options={options.materialOptions}
+            optionName={"material"}
+            isPriced={true}
+            selectedOptions={selectedOptions}
+            onSelect={onSelectOption}
+          />
+        );
       default:
         return null;
+    }
+  };
+
+  const reset = () => {
+    setShirtName("")
+    setActiveTab("Size")
+    setSelectedOptions(baseSelectedOption);
+    setPrice(basePrice)
+  }
+
+  const createShirt = async () => {
+    try {
+      for (const option of Object.keys(selectedOptions)) {
+        if (!selectedOptions[option]) {
+          alert(`Please select the ${option} for your T-shirt!`);
+          return;
+        }
+      }
+
+      const shirt = {
+        name: shirtName,
+        ...selectedOptions,
+      };
+
+      console.log(shirt)
+
+      const response = await fetch("http://localhost:3001/api/shirts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(shirt),
+      });
+
+      reset();
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -87,7 +197,9 @@ const AddShirt = () => {
             placeholder="Custom Shirt Name"
             className="input input-bordered text-black"
           />
-          <button className="btn bg-red-600 text-white hover:bg-black">Create</button>
+          <button className="btn bg-red-600 text-white hover:bg-black" onClick={createShirt}>
+            Create
+          </button>
         </div>
       </div>
 
